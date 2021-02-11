273db05b395e91d5b8130ba42c39d2d876420478
kraem <ebrin.ronnie@protonmail.com>
2021-02-11 19:11:53 +0100

    add ursa hardware

--- hardware/ursa-extra.nix
+++ hardware/ursa-extra.nix
@@ -30,6 +30,15 @@
 
   powerManagement.enable = false;
 
+  # TODO
+  # not yet working correctly with hashcat
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages = with pkgs; [
+    rocm-opencl-icd
+    rocm-opencl-runtime
+  ];
+
   imports = [
     ./bluetooth
   ];
