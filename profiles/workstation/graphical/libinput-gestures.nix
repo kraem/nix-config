544d7c47d61c0ba4626daa3996a5836ed23189a2
kraem <ebrin.ronnie@protonmail.com>
2021-02-14 18:53:39 +0100

    libinput-gestures: start on login

--- profiles/workstation/graphical/libinput-gestures.nix
+++ profiles/workstation/graphical/libinput-gestures.nix
@@ -1,23 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  # TODO extract, rename and activate through module?
   systemd.user.services.libinput-gestures = {
     serviceConfig = {
       Restart = "on-failure";
     };
-    wantedBy = [ "multi-user.target" ];
+    wantedBy = [ "graphical-session.target" ];
     environment = {
       DISPLAY = ":0";
     };
     script = ''
-
-      # Is it best to inline the scripts here or should we call the scripts?
-      # We win a wrapping bash process if inlining
-      #/home/kraem/bin/libinput-gestures/libinput-gestures.sh
-
       set -e
-
       libinput-gestures
     '';
     path = [ pkgs.bash pkgs.libinput-gestures pkgs.dbus ];
