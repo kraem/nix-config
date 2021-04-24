372917cad5c8b0c2db41ff0dda7f29aed7435561
kraem <ebrin.ronnie@protonmail.com>
2021-04-24 09:35:04 +0200

    graphical: dunst: simple formatting

--- profiles/workstation/graphical/dunst.nix
+++ profiles/workstation/graphical/dunst.nix
@@ -6,6 +6,13 @@
   home-manager.users.kraem = { ... }: {
     services.dunst = {
       enable = true;
+      settings = {
+          global = {
+          max_icon_size = 16;
+          geometry = "500x10-10+35";
+          format="<b>%s</b>\\n%b";
+        };
+      };
     };
   };
 }
