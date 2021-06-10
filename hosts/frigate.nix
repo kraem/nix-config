52b264fc0424bc820d1d4602c1ec19cd0c58944f
kraem <ebrin.ronnie@protonmail.com>
2021-06-10 19:35:44 +0200

    frigate: pipewire..

--- hosts/frigate.nix
+++ hosts/frigate.nix
@@ -30,6 +30,25 @@ in
       ../modules/wireguard/client
     ];
 
+  # Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
+  sound.enable = lib.mkForce false;
+  hardware.pulseaudio.enable = lib.mkForce false;
+
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
   # TODO: leave syncthingDir as the default syncthing module dir
   # and use impermanence instead
   my.syncthing = {
